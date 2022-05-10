5217123c8d054198f0eaa77b0ca739bc8432a0bb
Cecilia Lau <cecilialau6776@gmail.com>
2022-05-10 19:12:14 -0400

    Refactor rendering

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod tetris;
 
 extern crate sdl2;
 
-use crate::tetris::{Piece, Tetris};
+use crate::tetris::{Piece, Tetris, BOARD_HEIGHT, BOARD_WIDTH};
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::mouse::MouseButton;
@@ -13,7 +13,7 @@ use sdl2::render::{Canvas, Texture, TextureCreator};
 use sdl2::video::{Window, WindowContext};
 use std::collections::HashMap;
 
-const TILE_SIZE: u32 = 16;
+const TILE_SIZE: u32 = 32;
 
 const T_COLOR: Color = Color::RGB(161, 50, 240);
 const I_COLOR: Color = Color::RGB(0, 183, 235);
@@ -23,70 +23,70 @@ const Z_COLOR: Color = Color::RGB(220, 20, 60);
 const S_COLOR: Color = Color::RGB(50, 205, 50);
 const O_COLOR: Color = Color::RGB(255, 223, 0);
 
-fn generate_texture_map<'a>(
+fn render(
     canvas: &mut Canvas<Window>,
-    texture_creator: &'a TextureCreator<WindowContext>,
-) -> Result<HashMap<Piece, Texture<'a>>, String> {
-    let mut t_texture = texture_creator
-        .create_texture_target(None, TILE_SIZE * 3, TILE_SIZE * 2)
-        .map_err(|e| e.to_string())?;
-    let mut i_texture = texture_creator
-        .create_texture_target(None, TILE_SIZE * 4, TILE_SIZE * 1)
-        .map_err(|e| e.to_string())?;
-    let mut l_texture = texture_creator
-        .create_texture_target(None, TILE_SIZE * 3, TILE_SIZE * 2)
-        .map_err(|e| e.to_string())?;
-    let mut j_texture = texture_creator
-        .create_texture_target(None, TILE_SIZE * 3, TILE_SIZE * 2)
-        .map_err(|e| e.to_string())?;
-    let mut z_texture = texture_creator
-        .create_texture_target(None, TILE_SIZE * 3, TILE_SIZE * 2)
-        .map_err(|e| e.to_string())?;
-    let mut s_texture = texture_creator
-        .create_texture_target(None, TILE_SIZE * 3, TILE_SIZE * 2)
-        .map_err(|e| e.to_string())?;
-    let mut o_texture = texture_creator
-        .create_texture_target(None, TILE_SIZE * 2, TILE_SIZE * 2)
-        .map_err(|e| e.to_string())?;
-
-    {
-        let textures = vec![
-            (&mut t_texture, Piece::T),
-            (&mut i_texture, Piece::I),
-            (&mut l_texture, Piece::L),
-            (&mut j_texture, Piece::J),
-            (&mut z_texture, Piece::Z),
-            (&mut s_texture, Piece::S),
-            (&mut o_texture, Piece::O),
-        ];
-        match canvas.with_multiple_texture_canvas(textures.iter(), |texture_canvas, piece| {
-            texture_canvas.set_draw_color(Color::RGBA(0, 0, 0, 0));
-            texture_canvas.clear();
-            match *piece {
-                Piece::T => texture!(texture_canvas, T_COLOR, TILE_SIZE),
-                Piece::I => texture!(texture_canvas, I_COLOR, TILE_SIZE),
-                Piece::J => texture!(texture_canvas, J_COLOR, TILE_SIZE),
-                Piece::L => texture!(texture_canvas, L_COLOR, TILE_SIZE),
-                Piece::S => texture!(texture_canvas, S_COLOR, TILE_SIZE),
-                Piece::Z => texture!(texture_canvas, Z_COLOR, TILE_SIZE),
-                Piece::O => texture!(texture_canvas, O_COLOR, TILE_SIZE),
-                Piece::None => {}
-            }
-        }) {
-            Ok(()) => {}
-            Err(e) => return Err(e.to_string()),
-        };
+    texture_creator: &TextureCreator<WindowContext>,
+    board_background_texture: &Texture,
+    game_boards: &Vec<Tetris>,
+) -> Result<(), String> {
+    // create textures for the boards
+    let mut board_textures: Vec<Texture> = Vec::new();
+    for _ in game_boards.iter() {
+        let b = texture_creator
+            .create_texture_target(
+                None,
+                4 + tetris::BOARD_WIDTH as u32 * TILE_SIZE,
+                4 + tetris::BOARD_HEIGHT as u32 * TILE_SIZE,
+            )
+            .map_err(|e| e.to_string())?;
+        board_textures.push(b);
     }
+    let mut iter_vec: Vec<(&mut Texture, &Tetris)> = Vec::new();
+    for t in board_textures.iter_mut().zip(game_boards.iter()) {
+        iter_vec.push(t);
+    }
+    canvas
+        .with_multiple_texture_canvas(iter_vec.iter(), |board_canvas, game| {
+            game.draw_board_texture(board_canvas, 2, 2)
+                .expect("couldn't draw board");
+        })
+        .map_err(|e| e.to_string())?;
 
-    let mut texture_map = HashMap::new();
-    texture_map.insert(Piece::T, t_texture);
-    texture_map.insert(Piece::I, i_texture);
-    texture_map.insert(Piece::L, l_texture);
-    texture_map.insert(Piece::J, j_texture);
-    texture_map.insert(Piece::Z, z_texture);
-    texture_map.insert(Piece::S, s_texture);
-    texture_map.insert(Piece::O, o_texture);
-    Ok(texture_map)
+    // copy boards onto canvas
+    let mut i = 0;
+    let mut dst = Rect::new(
+        10,
+        10,
+        BOARD_WIDTH as u32 * TILE_SIZE + 4,
+        BOARD_HEIGHT as u32 * TILE_SIZE + 4,
+    );
+    for board_texture in board_textures.iter() {
+        // if i > 0 {
+        //     dst = Rect::new(0, 0, 1, 1);
+        // }
+        canvas.copy(
+            board_background_texture,
+            None,
+            Rect::new(
+                10,
+                10,
+                BOARD_WIDTH as u32 * TILE_SIZE + 4,
+                BOARD_HEIGHT as u32 * TILE_SIZE + 4,
+            ),
+        )?;
+        canvas.copy(
+            board_texture,
+            None,
+            Rect::new(
+                10,
+                10,
+                BOARD_WIDTH as u32 * TILE_SIZE + 4,
+                BOARD_HEIGHT as u32 * TILE_SIZE + 4,
+            ),
+        )?;
+        // i += 1;
+    }
+    Ok(())
 }
 
 pub fn main() -> Result<(), String> {
@@ -112,11 +112,46 @@ pub fn main() -> Result<(), String> {
     canvas.present();
 
     let texture_creator: TextureCreator<_> = canvas.texture_creator();
-    let textureMap = generate_texture_map(&mut canvas, &texture_creator)?;
-    let mut game = Tetris::new();
+
+    let mut board_background_texture = texture_creator
+        .create_texture_target(
+            None,
+            4 + tetris::BOARD_WIDTH as u32 * TILE_SIZE,
+            4 + tetris::BOARD_HEIGHT as u32 * TILE_SIZE,
+        )
+        .map_err(|e| e.to_string())?;
+    canvas
+        .with_texture_canvas(&mut board_background_texture, |tex| {
+            tex.set_draw_color(Color::RGBA(0, 0, 0, 0));
+            tex.clear();
+            tex.set_draw_color(Color::RGBA(30, 30, 30, 0));
+            for line in 0..=BOARD_HEIGHT {
+                tex.fill_rect(Rect::new(
+                    0,
+                    (line as u32 * TILE_SIZE) as i32 - 1 + 2,
+                    BOARD_WIDTH as u32 * TILE_SIZE,
+                    2,
+                ))
+                .expect("could not draw board bg rect");
+            }
+            for col in 0..=BOARD_WIDTH {
+                tex.fill_rect(Rect::new(
+                    (col as u32 * TILE_SIZE) as i32 - 1 + 2,
+                    0,
+                    2,
+                    BOARD_HEIGHT as u32 * TILE_SIZE,
+                ))
+                .expect("could not draw board bg rect");
+            }
+        })
+        .map_err(|e| e.to_string())?;
+    // let texture_map = generate_texture_map(&mut canvas, &texture_creator)?;
+
+    let mut boards = vec![Tetris::new()];
 
     let mut event_pump = sdl_context.event_pump()?;
     let mut frame: u32 = 0;
+    let mut scale: f32 = 3.0;
     'running: loop {
         // handle events
         for event in event_pump.poll_iter() {
@@ -132,33 +167,41 @@ pub fn main() -> Result<(), String> {
                     repeat: false,
                     ..
                 } => {
-                    game.toggle_state();
+                    boards[0].spawn_next();
+                    // boards[0].toggle_state();
+                }
+
+                Event::KeyDown {
+                    keycode: Some(Keycode::S),
+                    repeat: false,
+                    ..
+                } => {
+                    scale -= 0.1;
+                }
+
+                Event::KeyDown {
+                    keycode: Some(Keycode::W),
+                    repeat: false,
+                    ..
+                } => {
+                    scale += 0.1;
                 }
 
                 _ => {}
             }
         }
 
-        // draw board
-        canvas.set_draw_color(Color::RGB(0, 0, 0));
-        canvas.clear();
-        for (x, row) in game.rows_iter().enumerate() {
-            for (y, piece) in row.enumerate() {
-                if let Some(texture) = textureMap.get(piece) {
-                    canvas.copy(
-                        texture,
-                        None,
-                        Rect::new(
-                            (x as u32 * TILE_SIZE) as i32,
-                            (y as u32 * TILE_SIZE) as i32,
-                            TILE_SIZE,
-                            TILE_SIZE,
-                        ),
-                    )?;
-                }
-            }
-        }
+        render(
+            &mut canvas,
+            &texture_creator,
+            &board_background_texture,
+            &boards,
+        )?;
+        // canvas.clear();
+        // boards[0].spawn_next();
+        // boards[0].draw_board_texture(&mut canvas, 10, 10)?;
         canvas.present();
+        // break;
     }
 
     Ok(())
--- src/tetris.rs
+++ src/tetris.rs
@@ -1,10 +1,12 @@
-use array2d::Array2D;
 use rand::{prelude::*, seq::SliceRandom};
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+use sdl2::render::Canvas;
+use sdl2::video::Window;
 use std::collections::VecDeque;
 
-pub const SQUARE_SIZE: u32 = 16;
-pub const BOARD_HEIGHT: u8 = 24;
-pub const BOARD_WIDTH: u8 = 10;
+pub const BOARD_HEIGHT: usize = 24;
+pub const BOARD_WIDTH: usize = 10;
 const PIECES: [Piece; 7] = [
     Piece::T,
     Piece::I,
@@ -21,7 +23,7 @@ pub enum State {
     Playing,
 }
 
-#[derive(Clone, PartialEq, Eq, Hash)]
+#[derive(Clone, PartialEq, Eq, Hash, Copy, Debug)]
 pub enum Piece {
     T,
     I,
@@ -35,36 +37,45 @@ pub enum Piece {
 
 #[derive(Clone)]
 pub struct Tetris {
-    board: Array2D<Piece>,
+    board: [[Piece; BOARD_WIDTH]; BOARD_HEIGHT],
+    row_counts: [u8; BOARD_HEIGHT],
+    line_map: [usize; BOARD_HEIGHT],
     queue: VecDeque<Piece>,
+    active_piece: Piece,
+    col_active: usize,
+    line_active: usize,
     state: State,
     rng: StdRng,
 }
 
 impl Tetris {
     pub fn new() -> Tetris {
-        let mut board = Array2D::filled_with(Piece::None, BOARD_HEIGHT as usize, BOARD_WIDTH as usize);
+        // init line map
+        let mut line_map = [0; BOARD_HEIGHT];
+        for i in 0..BOARD_HEIGHT {
+            line_map[i] = i;
+        }
+
+        // Fill initial queue
         let mut queue: VecDeque<Piece> = VecDeque::new();
         let mut rng: StdRng = SeedableRng::from_entropy();
-
         let mut pieces_clone = PIECES.clone();
         pieces_clone.shuffle(&mut rng);
         queue.append(&mut VecDeque::from_iter(pieces_clone));
         let mut pieces_clone = PIECES.clone();
         pieces_clone.shuffle(&mut rng);
         queue.append(&mut VecDeque::from_iter(pieces_clone));
-
-        // test code
-        board.set(2, 3, Piece::T);
-        board.set(3, 2, Piece::T);
-        board.set(3, 3, Piece::T);
-        board.set(3, 4, Piece::T);
 
         Tetris {
-            board,
+            board: [[Piece::None; BOARD_WIDTH]; BOARD_HEIGHT],
+            row_counts: [0; BOARD_HEIGHT],
+            line_map,
             queue,
             state: State::Playing,
             rng,
+            active_piece: Piece::None,
+            col_active: 0,
+            line_active: 0,
         }
     }
 
@@ -75,8 +86,8 @@ impl Tetris {
         }
     }
 
-    pub fn get(&self, line: u8, col: u8) -> Option<&Piece> {
-        self.board.get(line as usize, col as usize)
+    pub fn get(&self, line: usize, col: usize) -> Piece {
+        self.board[self.line_map[line]][col]
     }
 
     fn queue_add_bag(&mut self) {
@@ -85,7 +96,90 @@ impl Tetris {
         self.queue.append(&mut VecDeque::from_iter(pieces_clone));
     }
 
-    pub fn rows_iter(&self) -> impl Iterator<Item = impl Iterator<Item = &Piece>> {
-        self.board.rows_iter()
+    pub fn draw_board_texture(
+        &self,
+        texture_canvas: &mut Canvas<Window>,
+        x_offset: i32,
+        y_offset: i32,
+    ) -> Result<(), String> {
+        texture_canvas.set_draw_color(Color::RGBA(0, 0, 0, 0));
+        texture_canvas.clear();
+        println!("{:?}", self.board[self.line_map[1]][4]);
+        for line in 0..BOARD_HEIGHT {
+            for col in 0..BOARD_WIDTH {
+                match self.board[self.line_map[line]][col] {
+                    Piece::T => texture_canvas.set_draw_color(crate::T_COLOR),
+                    Piece::I => texture_canvas.set_draw_color(crate::I_COLOR),
+                    Piece::J => texture_canvas.set_draw_color(crate::J_COLOR),
+                    Piece::L => texture_canvas.set_draw_color(crate::L_COLOR),
+                    Piece::S => texture_canvas.set_draw_color(crate::S_COLOR),
+                    Piece::Z => texture_canvas.set_draw_color(crate::Z_COLOR),
+                    Piece::O => texture_canvas.set_draw_color(crate::O_COLOR),
+                    Piece::None => continue,
+                }
+                println!("{:?}", texture_canvas.draw_color().rgb());
+                texture_canvas.fill_rect(Rect::new(
+                    x_offset + (col as u32 * crate::TILE_SIZE) as i32,
+                    y_offset + (line as u32 * crate::TILE_SIZE) as i32,
+                    crate::TILE_SIZE,
+                    crate::TILE_SIZE,
+                ))?;
+            }
+        }
+        Ok(())
+    }
+
+    // TODO: remove pub
+    pub fn spawn_next(&mut self) {
+        // TODO: check for top-out
+        self.active_piece = self.queue.pop_front().unwrap_or(Piece::None);
+        self.col_active = 4;
+        self.line_active = 1;
+        match self.active_piece {
+            Piece::T => {
+                self.board[self.line_map[0]][4] = self.active_piece;
+                self.board[self.line_map[1]][3] = self.active_piece;
+                self.board[self.line_map[1]][4] = self.active_piece;
+                self.board[self.line_map[1]][5] = self.active_piece;
+            }
+            Piece::I => {
+                self.board[self.line_map[1]][3] = self.active_piece;
+                self.board[self.line_map[1]][4] = self.active_piece;
+                self.board[self.line_map[1]][5] = self.active_piece;
+                self.board[self.line_map[1]][6] = self.active_piece;
+            }
+            Piece::J => {
+                self.board[self.line_map[0]][3] = self.active_piece;
+                self.board[self.line_map[1]][3] = self.active_piece;
+                self.board[self.line_map[1]][4] = self.active_piece;
+                self.board[self.line_map[1]][5] = self.active_piece;
+            }
+            Piece::L => {
+                self.board[self.line_map[0]][5] = self.active_piece;
+                self.board[self.line_map[1]][3] = self.active_piece;
+                self.board[self.line_map[1]][4] = self.active_piece;
+                self.board[self.line_map[1]][5] = self.active_piece;
+            }
+            Piece::S => {
+                self.board[self.line_map[0]][4] = self.active_piece;
+                self.board[self.line_map[0]][5] = self.active_piece;
+                self.board[self.line_map[1]][3] = self.active_piece;
+                self.board[self.line_map[1]][4] = self.active_piece;
+            }
+            Piece::Z => {
+                self.board[self.line_map[0]][3] = self.active_piece;
+                self.board[self.line_map[0]][4] = self.active_piece;
+                self.board[self.line_map[1]][4] = self.active_piece;
+                self.board[self.line_map[1]][5] = self.active_piece;
+            }
+            Piece::O => {
+                self.board[self.line_map[0]][4] = self.active_piece;
+                self.board[self.line_map[0]][5] = self.active_piece;
+                self.board[self.line_map[1]][4] = self.active_piece;
+                self.board[self.line_map[1]][5] = self.active_piece;
+            }
+            Piece::None => {}
+        }
+        println!("spawn piece {:?}", self.active_piece);
     }
 }
